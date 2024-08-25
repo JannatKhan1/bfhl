6441a0c907f4854a5467367395559f5ab7bfb0c0
Jannat <jannatfbf@gmail.com>
2024-08-25 13:25:38 +0530

    modified:   index.js

--- index.js
+++ index.js
@@ -13,7 +13,10 @@ const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
-app.use(cors())
+app.use(cors({
+    methods: ['GET', 'POST'], // Allow specific methods
+    allowedHeaders: ['Content-Type'],
+}))
 
 //Routes
 app.use('/api/user/',require('./routes/userRoutes'))
